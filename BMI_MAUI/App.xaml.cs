374999a0b0611b51d9eb1f7a10ff49f6457035ec
Iyan Pathan <iyanpathan@Iyans-MacBook-Air.local>
2023-05-21 22:01:05 +0530

    initial commit

---
+++ BMI_MAUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace BMI_MAUI;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ BMI_MAUI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BMI_MAUI;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
